5cfc8673d9a65944f1cab57ce9d3bfd231327118
astaweni <116157192+astaweni@users.noreply.github.com>
2022-10-19 20:55:17 +0700

    Update acardia.js

--- acardia.js
+++ acardia.js
@@ -1,4 +1,3 @@
-<script type='text/javascript'>
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
@@ -7,13 +6,3 @@ const username = loginForm.username.value;
 const password = loginForm.password.value;
 if (username === "anggota" && password === "1235") {window.open('https://sites.google.com/view/koperasi-mandiri/tagihan', '_blank');}
 else {alert("Salah Gais, coba lagi");}})
-</script>
-<div id="njobo" style="text-align:center;">
-<div style="height:100%;">
-<form id="login-form">
-<input type="text" name="username" id="username-field" placeholder="Username" style="width:50%;"><br/><br/>
-<input type="password" name="password" id="password-field" placeholder="Password" style="width:50%;"><br/><br/>
-<input type="submit" value="Masuk Gan" id="login-form-submit" style="width:50%;">
-</form>
-</div>
-</div>
